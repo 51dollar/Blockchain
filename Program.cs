2736678d39ef8b1ac32490da7ac25accc13672c1
Dmitry <borushko.dmitry@gmail.com>
2025-07-20 20:46:18 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace Blockchain;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
