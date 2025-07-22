462c208ae7fd0c733245fb3f1b7f19be862da636
Dmitry <borushko.dmitry@gmail.com>
2025-07-22 19:45:55 +0300

    Add Ui data

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,54 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        button1 = new Button();
+        textBox1 = new TextBox();
+        listBox = new ListBox();
+        SuspendLayout();
+        // 
+        // button1
+        // 
+        button1.Location = new Point(713, 12);
+        button1.Name = "button1";
+        button1.Size = new Size(75, 23);
+        button1.TabIndex = 0;
+        button1.Text = "Добавить";
+        button1.UseVisualStyleBackColor = true;
+        button1.Click += button1_Click;
+        // 
+        // textBox1
+        // 
+        textBox1.Location = new Point(12, 12);
+        textBox1.Name = "textBox1";
+        textBox1.Size = new Size(680, 23);
+        textBox1.TabIndex = 1;
+        // 
+        // listBox
+        // 
+        listBox.FormattingEnabled = true;
+        listBox.Location = new Point(12, 41);
+        listBox.Name = "listBox";
+        listBox.Size = new Size(776, 394);
+        listBox.TabIndex = 2;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Controls.Add(listBox);
+        Controls.Add(textBox1);
+        Controls.Add(button1);
+        Name = "Form1";
+        Text = "Form1";
+        Load += Form1_Load;
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private Button button1;
+    private TextBox textBox1;
+    private ListBox listBox;
 }
--- Form1.cs
+++ Form1.cs
@@ -6,4 +6,26 @@ public partial class Form1 : Form
     {
         InitializeComponent();
     }
+
+    private Chain _chain = new Chain();
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        listBox.Items.Clear();
+
+        _chain.Add(textBox1.Text, "Admin");
+
+        foreach (var block in _chain.Blocks)
+        {
+            listBox.Items.Add(block);
+        }
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        foreach (var block in _chain.Blocks)
+        {
+            listBox.Items.Add(block);
+        }
+    }
 }
